7c1cab1a8ba36ac9ac958dc70b7ea4a41e463e91
Ben Scholer <ben.scholer@dronedeploy.com>
2024-05-11 19:54:06 -0400

    add MIRROR_IMAGES, prepend random string, use main function

--- reset_timestamps.py
+++ reset_timestamps.py
@@ -1,15 +1,24 @@
 import os
 import piexif
 from datetime import datetime
+from PIL import Image
 import glob
 from tqdm import tqdm
 
 # Set the directory path to your folder containing JPEG images
 directory_path = '/Volumes/SCANS/DCIM/100COACH'
+MIRROR_IMAGES = True
 
 
 def encode_exif_date(date_str):
     return date_str.encode('utf-8')  # Ensure the date is in the correct byte format
+
+
+def mirror_image(file_path):
+    with Image.open(file_path) as img:
+        mirrored_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+        mirrored_img.save(file_path)
+        print(f"Mirrored image saved for {file_path}")
 
 
 def update_capture_date(file_path):
@@ -33,10 +42,26 @@ def update_capture_date(file_path):
         print(f"Error updating EXIF data for {file_path}: {e}")
 
 
-for root, dirs, files in os.walk(directory_path):
-    for file in files:
-        if file.lower().endswith(('.jpg', '.jpeg')) and not file.startswith('._'):
-            file_path = os.path.join(root, file)
-            update_capture_date(file_path)
-        else:
-            print(f"Skipping non-image or system file: {file}")
+def prepend_random_string(file_paths):
+    random_string = os.urandom(2).hex()
+    for file_path in file_paths:
+        new_file_path = os.path.join(os.path.dirname(file_path), f"{random_string}_{os.path.basename(file_path)}")
+        os.rename(file_path, new_file_path)
+        print(f"Renamed {file_path} to {new_file_path}")
+
+
+def main():
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.lower().endswith(('.jpg', '.jpeg')) and not file.startswith('._'):
+                file_path = os.path.join(root, file)
+                update_capture_date(file_path)
+                if MIRROR_IMAGES:
+                    mirror_image(file_path)
+                prepend_random_string([file_path])
+            else:
+                print(f"Skipping non-image or system file: {file}")
+
+
+if __name__ == '__main__':
+    main()
